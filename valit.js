21632a0eac78912ae50e2b1dae98626ef97bb3e0
Nathanael McMillan <nmcmillan@perfectcolours.com>
2015-06-05 17:18:12 +0100

    Postcode lottery
    Added a postcode reggae, which may be suitable but will still let
    through some non postcodes and needs more testing.
    
    Added a new valit-len for character length add like so [no of char] |
    [message].
    
    Same for phone number, removed the need for hard coding in the jQuery
    plugin. Add the length in the valit-tel att

--- valit.js
+++ valit.js
@@ -14,25 +14,24 @@
 			parent_error_class: 'danger',
 			add_success_to_parent: true,
 			parent_success_class: 'success',
-			telephone_length: 11,
 			trigger: 'focusout'
 		}, options );
 			
 		return this.on(defaults.trigger, this, function (e) 
 		{			
 		
-		// DELETE THESE
-			var valit_required				=	$(this).data('valit-required');
-			var valit_email					=	$(this).data('valit-email');
-			var valit_num					=	$(this).data('valit-num');
-			var valit_tel					=	$(this).data('valit-tel');
-		//
-			
-			var valit_ele_id 				= 	$(this).attr('id'); // Grab the ID of the element
-			var valit_label					=	$(document).find(' label[for="' + valit_ele_id + '"] ');
+			var valit_required		=	$(this).data('valit-required');
+			var valit_email			=	$(this).data('valit-email');
+			var valit_num			=	$(this).data('valit-num');
+			var valit_tel			=	$(this).data('valit-tel');
+			var valit_postcode		=	$(this).data('valit-postcode');
+			var valit_length			=	$(this).data('valit-len');
+	
+			var valit_ele_id 		= 	$(this).attr('id'); // Grab the ID of the element
+			var valit_label			=	$(document).find(' label[for="' + valit_ele_id + '"] ');
 		
-			var pass_validation 				=	true;
-			var error_message 				=	'';
+			var pass_validation 		=	true;
+			var error_message 		=	[];
 			
 			if ( '[data-element^="valit-"]' ) // Check to see if the element has the data attribute
 			{
@@ -43,7 +42,7 @@
 					if($(this).val().length==0)
 					{
 						pass_validation = false;
-						error_message += valit_required;
+						error_message.push(valit_required);
 					} 
 				}
 				
@@ -54,7 +53,7 @@
 					if( !email_regex.test( $(this).val() ) )
 					{
 						pass_validation = false;
-						error_message  += valit_email;
+						error_message.push(valit_email);
 					} 
 				}
 				
@@ -63,19 +62,41 @@
 					if ( !validate_number( $(this).val() ) )
 					{
 						pass_validation = false;
-						error_message  += valit_num;
+						error_message.push(valit_num);
 					}
 				}
 				
 				if ( $(this).data('valit-tel') )
 				{
-					console.log($(this).val().length );
-					
-					if ( !validate_number( $(this).val() ) || $(this).val().length < defaults.telephone_length ) 
+					var char_len = valit_tel.split('|');	
+					if ( !validate_number( $(this).val() ) || $(this).val().length < char_len[0] ) 
 					{
 						pass_validation = false;
-						error_message  += valit_tel;
+						error_message.push(char_len[1]);
 					}
+				}
+				
+				if ( $(this).data('valit-postcode') )
+				{
+					var poscode_regex = /^(GIR 0AA)|((([ABCDEFGHIJKLMNOPRSTUWYZ][0-9][0-9]?)|(([ABCDEFGHIJKLMNOPRSTUWYZ][ABCDEFGHKLMNOPQRSTUVWXY][0-9][0-9]?)|(([ABCDEFGHIJKLMNOPRSTUWYZ][0-9][ABCDEFGHJKSTUW])|([ABCDEFGHIJKLMNOPRSTUWYZ][ABCDEFGHKLMNOPQRSTUVWXY][0-9][ABEHMNPRVWXY])))) [0-9][ABDEFGHJLNPQRSTUWXYZ]{2})/;
+					
+					if( !poscode_regex.test( $(this).val() ) )
+					{
+						pass_validation = false;
+						error_message.push(valit_postcode);
+					} 
+						
+				}
+				
+				if ($(this).data('valit-len'))
+				{			
+				
+					var char_len = valit_length.split('|');	
+					if($(this).val().length < char_len[0])
+					{
+						pass_validation = false;
+						error_message.push(char_len[1]);
+					} 
 				}
 								
 			
@@ -95,9 +116,11 @@
 						// Remove any of the success classes if this fails after a successful pass
 						$(this).removeClass(defaults.input_success_class);
 						$(this).parent().removeClass(defaults.parent_success_class);
-	
-						// Errors need to be stepped through
-						valit_label.append('<' + defaults.wrap_error_message + ' class ="' + defaults.wrap_error_class +'">' + error_message + '</' + defaults.wrap_error_message + '> ');
+						
+						// Loop through error message array
+						$.each(error_message, function( i ) {
+						  valit_label.append('<'+ defaults.wrap_error_message + ' class ="' + defaults.wrap_error_class +'">'+ error_message[i] +'</' + defaults.wrap_error_message + '>');
+						});
 					}
 				} 
 				else
